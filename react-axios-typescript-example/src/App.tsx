aeebae6ac85c940a4ce8f177a5d22f2a4c15f1e2
alvintsui0203 <yanwingtsui0203@gmail.com>
2023-02-17 10:35:47 -0500

    Add nav bar, home page and routing

--- react-axios-typescript-example/src/App.tsx
+++ react-axios-typescript-example/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import LoginBox from './components/LoginPage/LoginBox'
-
+import Home from './components/HomePage/Home';
 import './App.css';
 
 function App() {
   return (
-    < LoginBox/>
+    < Home/>
   );
 }
 
