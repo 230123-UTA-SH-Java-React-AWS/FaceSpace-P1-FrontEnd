89975b89b441338d1e05f2036c8923173e595135
alvintsui0203 <yanwingtsui0203@gmail.com>
2023-02-17 20:10:06 -0500

    add Post html

--- react-axios-typescript-example/src/index.tsx
+++ react-axios-typescript-example/src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginBox from './components/LoginPage/LoginBox';
 import NavigationBar from './components/NavigationBar/NavigationBar';
+import Post from './components/Post/Post';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,6 +18,7 @@ root.render(
     <Routes>
       <Route path = "/" element = {<App />} />
       <Route path = "/Login" element = {<LoginBox />} />
+      <Route path = "/Post" element = {<Post />} />
     </Routes>
   </BrowserRouter>
 </React.StrictMode>
