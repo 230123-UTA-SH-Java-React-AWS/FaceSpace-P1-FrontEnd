fbffb1cd86107ac8aa7951526718982f1998eadd
sara-fr <sara.farhadi088@gmail.com>
2023-02-17 18:57:52 -0800

    Updating

--- react-axios-typescript-example/src/shared/Redux/store.ts
+++ react-axios-typescript-example/src/shared/Redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import UserSlice from "../../components/LoginPage/UserSlice";
+
 
 export const store = configureStore({
     reducer: {
-        
+        user: UserSlice
     }
 })
 
